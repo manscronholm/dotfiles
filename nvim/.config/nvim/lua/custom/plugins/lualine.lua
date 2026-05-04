512f3473b813a65c4d0d1c50f867fc1673c7bac1
Måns Cronholm <mans.cronholm@norionbank.se>
2026-05-04 08:11:14 +0200

    nvim: set lualine theme to auto

--- nvim/.config/nvim/lua/custom/plugins/lualine.lua
+++ nvim/.config/nvim/lua/custom/plugins/lualine.lua
@@ -1,11 +1,10 @@
-local options = require 'vim.filetype.options'
 return {
   'nvim-lualine/lualine.nvim',
   dependencies = { 'nvim-tree/nvim-web-devicons' },
   config = function()
     require('lualine').setup {
       options = {
-        theme = 'catppuccin',
+        theme = 'auto',
       },
     }
   end,
