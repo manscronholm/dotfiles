a1c7f4216eebe604e176f1e87804f7ef3062f7a3
Måns Cronholm <mans.cronholm@norionbank.se>
2026-05-04 08:51:56 +0200

    nvim: make selection commenting more consistent

--- nvim/.config/nvim/lua/custom/plugins/comment.lua
+++ nvim/.config/nvim/lua/custom/plugins/comment.lua
@@ -3,12 +3,18 @@ return {
   event = "VeryLazy",
   opts = {},
   config = function(_, opts)
+    local api = require("Comment.api")
+    local esc = vim.api.nvim_replace_termcodes("<Esc>", true, false, true)
+
     require("Comment").setup(opts)
+
     vim.keymap.set("n", "<leader>c", function()
-      require("Comment.api").toggle.linewise.current()
+      api.locked("toggle.linewise.current")()
     end, { desc = "[C]omment line" })
+
     vim.keymap.set("x", "<leader>c", function()
-      require("Comment.api").toggle.linewise(vim.fn.visualmode())
+      vim.api.nvim_feedkeys(esc, "nx", false)
+      api.locked("toggle.linewise")(vim.fn.visualmode())
     end, { desc = "[C]omment selection" })
   end,
 }
